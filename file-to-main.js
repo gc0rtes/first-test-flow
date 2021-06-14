38c100a6533aaed24383292a3697aaeaaa4f47b1
gc0rtes <guilhermeco@gmail.com>
2021-06-14 17:51:00 +0200

    first commit to dev

--- file-to-main.js
+++ file-to-main.js
@@ -4,4 +4,8 @@ JUST TOUCH THIS AT THE END OF THE PROJECT
 */
 /*
 Now I can code here and all the commit goes to the development branch
-and when I'm done I can merge on Git to the main branch */
+and when I'm done I can merge on Git to the main branch 
+
+asfdfasdfasd
+sadfa
+*/
